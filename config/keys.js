2bb3a5fb459f771d1ecc38838a6ae8210b168c30
Khushi Rawat <117963603+KhushiRawat29@users.noreply.github.com>
2024-06-30 11:24:36 +0530

    Create keys.js

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const MONGO_CONNECTION_URI="mongodb+srv://sakshivarshney311:Sakshi.311@learn-flow.qgcy9ig.mongodb.net/?retryWrites=true&w=majority&appName=Learn-flow";
+
+module.exports = {
+    MONGO_CONNECTION_URI,
+  };
